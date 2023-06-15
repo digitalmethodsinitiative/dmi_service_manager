d97db6190ee25aeb948cad3b67bed388c3ef7c79
Dale Wahl <dalewahl@gmail.com>
2023-06-15 10:18:49 +0200

    fix log -> logger references

--- api/shell_executor.py
+++ api/shell_executor.py
@@ -18,13 +18,13 @@ if config_data.get('4CAT_DATASETS_PATH', False):
     fourcat_path = Path(config_data.get('4CAT_DATASETS_PATH'))
     shell2http.register_command(endpoint="whisper_local", command_name=f"docker run -v {fourcat_path}:/app/data/ --gpus all whisper whisper")
 else:
-    app.log.warning("4CAT_DATASETS_PATH not set; whisper_local endpoint not available")
+    app.logger.warning("4CAT_DATASETS_PATH not set; whisper_local endpoint not available")
 
 if config_data.get('UPLOAD_FOLDER_PATH', False):
     uploads_path = Path(config_data.get('UPLOAD_FOLDER_PATH'))
     shell2http.register_command(endpoint="whisper_remote", command_name=f"docker run -v {uploads_path}:/app/data/ --gpus all whisper whisper")
 else:
-    app.log.warning("UPLOAD_FOLDER_PATH not set; whisper_remote endpoint not available")
+    app.logger.warning("UPLOAD_FOLDER_PATH not set; whisper_remote endpoint not available")
 
 if config_data.get('WHISPER_LIVE', False):
     shell2http.register_command(endpoint="whisper_live", command_name=f"docker exec whisper whisper")
