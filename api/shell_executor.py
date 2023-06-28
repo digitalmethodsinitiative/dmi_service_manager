f32e4f2bf908f24138499c700ce8c49e535a3ccf
Dale Wahl <dalewahl@gmail.com>
2023-06-28 13:07:16 +0200

    add clip

--- api/shell_executor.py
+++ api/shell_executor.py
@@ -13,15 +13,23 @@ shell2http = Shell2HTTP(app=app, executor=executor, base_url_prefix="/api/")
 
 # Register commands
 
-# Whisper
+# Local Endpoints
 if config_data.get('4CAT_DATASETS_PATH', False):
     fourcat_path = Path(config_data.get('4CAT_DATASETS_PATH'))
-    shell2http.register_command(endpoint="whisper_local", command_name=f"docker run --rm -v {fourcat_path}:/app/data/ {'--gpus all ' if config_data.get('WHISPER_GPU', False) else ''}whisper whisper")
-else:
-    app.logger.warning("4CAT_DATASETS_PATH not set; whisper_local endpoint not available")
 
+    # Whisper
+    shell2http.register_command(endpoint="whisper_local", command_name=f"docker run --rm -v {fourcat_path}:/app/data/ {'--gpus all ' if config_data.get('GPU_ENABLED', False) else ''}whisper whisper")
+    # CLIP
+    shell2http.register_command(endpoint="clip_local", command_name=f"docker run --rm -v {fourcat_path}:/app/data/ {'--gpus all ' if config_data.get('GPU_ENABLED', False) else ''}clip python3 clip_interface.py")
+else:
+    app.logger.warning("4CAT_DATASETS_PATH not set; local endpoints not available")
+
+# Remote Endpoints
 if config_data.get('UPLOAD_FOLDER_PATH', False):
     uploads_path = Path(config_data.get('UPLOAD_FOLDER_PATH'))
-    shell2http.register_command(endpoint="whisper_remote", command_name=f"docker run --rm -v {uploads_path}:/app/data/ {'--gpus all ' if config_data.get('WHISPER_GPU', False) else ''}whisper whisper")
+    shell2http.register_command(endpoint="whisper_remote", command_name=f"docker run --rm -v {uploads_path}:/app/data/ {'--gpus all ' if config_data.get('GPU_ENABLED', False) else ''}whisper whisper")
+    # CLIP
+    shell2http.register_command(endpoint="clip_remote",
+                                command_name=f"docker run --rm -v {uploads_path}:/app/data/ {'--gpus all ' if config_data.get('GPU_ENABLED', False) else ''}clip python3 clip_interface.py")
 else:
     app.logger.warning("UPLOAD_FOLDER_PATH not set; whisper_remote endpoint not available")
