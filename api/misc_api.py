0c24e282cc24812a7ac2ca7db5d99368a1749c73
Stijn Peeters <stijn.peeters@uva.nl>
2024-04-02 17:44:52 +0200

    Add image classification service

--- api/misc_api.py
+++ api/misc_api.py
@@ -16,7 +16,7 @@ def check_gpu_mem(service_id):
     if not config_data.get('GPU_ENABLED', False):
         return jsonify({'reason': 'GPU not enabled on this instance of DMI Service Manager'}), 400
 
-    if service_id not in ['whisper', 'clip', 'stable_diffusion', 'stormtrooper']:
+    if service_id not in ['whisper', 'clip', 'stable_diffusion', 'stormtrooper', 'image_classification']:
         # TODO: manage services interactively
         return jsonify({'reason': 'Service not found'}), 404
 
